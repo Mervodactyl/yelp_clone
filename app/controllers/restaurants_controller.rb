bb172d337c0b7f1a58562e42182772f703cac6f4
Mervodactylgit config --global user.name Mervodactylgit config --global user.name Mervodactyl <merve.silk@gmail.com>
2014-10-01 17:30:41 +0100

    added delete and update etc

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,44 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    Restaurant.create(params[:restaurant].permit(:name))
+    redirect_to '/restaurants'
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(params[:restaurant].permit(:name))
+
+    redirect_to '/restaurants'
+  end
+
+  def destroy
+    restaurant = Restaurant.find(params[:id])
+    restaurant.destroy
+
+    flash[:notice] = 'Restaurant deleted successfully'
+    redirect_to '/restaurants'
+  end
+
 end
+
+# iterm
+
+# bin/rails g migration add_promoted_to_restaurants promoted:boolean
+
+# bin/rake db:migrate
+
+# psql yelp_clone_development
+
+# \d restaurants
+
+# \q
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,19 +1,17 @@
 require 'rails_helper'
 
-describe 'restaurants' do
+context 'restaurants' do
 
-  context 'no restaurants have been added' do
-
+  describe 'no restaurants have been added' do
     it 'should display a prompt to add a restaurant' do
-      visit '/restaurants'
-      expect(page).to have_content 'No restaurants'
-      expect(page).to have_link 'Add a restaurant'
+        visit '/restaurants'
+        expect(page).to have_content 'No restaurants'
+        expect(page).to have_link 'Add a restaurant'
+      end
     end
 
-  end
-
-  context 'restaurants have been added' do
-    before do
+  describe 'restaurants have been added' do
+    before (:each) do
       Restaurant.create(name: 'KFC')
     end
 
@@ -23,6 +21,45 @@ describe 'restaurants' do
       expect(page).not_to have_content('No restaurants added yet')
     end
   end
-end
 
+  describe 'creating restaurants' do
+    it 'prompts the user to fill out a form, then displays the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
+  describe 'editing restaurants' do
+      before (:each) do
+        Restaurant.create(name: 'McDonalds')
+      end
+
+    it 'prompts the user to edit the restaurant name' do
+      visit '/restaurants'
+      click_link 'Edit McDonalds'
+      fill_in 'Name', with: 'McDonalds'
+      click_button 'Update Restaurant'
+      expect(page).to have_content 'McDonalds'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
+  describe 'editing restaurants' do
+      before (:each) do
+        Restaurant.create(name: 'McDonalds')
+      end
+
+      it 'removes the restaurants when a user clicks the delete link' do
+        visit '/restaurants'
+        click_link 'Delete McDonalds'
+        expect(page).not_to have_content 'McDonalds'
+        expect(page).to have_content 'Restaurant deleted successfully'
+        expect(current_path).to eq '/restaurants'
+      end
+  end
+end
 
