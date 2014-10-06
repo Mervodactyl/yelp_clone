cab346f480c1d8fbc4bf5ca6260021d4cd6e9a54
Mervodactylgit config --global user.name Mervodactylgit config --global user.name Mervodactyl <merve.silk@gmail.com>
2014-10-06 15:23:35 +0100

    added reviews and tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Restaurant.destroy_all
+Review.destroy_all
+
+
+KFC = Restaurant.create(name: 'KFC')
+KFC.reviews.create(rating: 1, thoughts: 'Bad')
+KFC.reviews.create(rating: 1, thoughts: 'Poor')
+KFC.reviews.create(rating: 1, thoughts: 'Ok')
+KFC.reviews.create(rating: 1, thoughts: 'Good')
+KFC.reviews.create(rating: 1, thoughts: 'Great')
+
+Nandos = Restaurant.create(name: 'Nandos')
+Nandos.reviews.create(rating: 1, thoughts: 'Really Bad')
+Nandos.reviews.create(rating: 1, thoughts: 'Really Poor')
+Nandos.reviews.create(rating: 1, thoughts: 'Really Ok')
+Nandos.reviews.create(rating: 1, thoughts: 'Really Good')
+Nandos.reviews.create(rating: 1, thoughts: 'Really Great')
+
+Wimpy = Restaurant.create(name: 'Wimpy')
+Wimpy.reviews.create(rating: 1, thoughts: 'Really Really Bad')
+Wimpy.reviews.create(rating: 1, thoughts: 'Really Really Poor')
+Wimpy.reviews.create(rating: 1, thoughts: 'Really Really Ok')
+Wimpy.reviews.create(rating: 1, thoughts: 'Really Really Good')
+Wimpy.reviews.create(rating: 1, thoughts: 'Really Really Great')
+
+
